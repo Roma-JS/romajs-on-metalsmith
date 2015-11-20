7db35cf5367f1d94cd1240d73c768bbcd61b8bd8
Luca Lanziani <luca@lanziani.com>
2015-11-20 18:29:15 +0100

    Add travis configuration and github pages deploy script
    The token generated ${GH_TOKEN} **is in my profile**
    we should move it on the Romajs user profile

---
+++ publish_on_github.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+OUTPUTDIR='build'
+GITHUB_USERNAME='Travis CI'
+GITHUB_EMAIL='luca@lanziani.com'
+GITHUB_MESSAGE='Travis deploy'
+GITHUB_REF='github.com:Roma-JS/roma-js.github.io.git'
+# go to the out directory and create a *new* Git repo
+cd $(OUTPUTDIR)
+git init
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "${GITHUB_USERNAME}"
+git config user.email "${GITHUB_EMAIL}"
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy to GitHub Pages".
+git add .
+git commit -m "${GITHUB_MESSAGE}"
+
+ifeq ($(TRAVIS_PULL_REQUEST), false)
+  # Force push from the current repo's master branch to the remote
+  # repo's gh-pages branch. (All previous history on the gh-pages branch
+  # will be lost, since we are overwriting it.) We redirect any output to
+  # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+  git push --force --quiet "https://${GH_TOKEN}@${GITHUB_REF}" master:master > /dev/null 2>&1
+endif
