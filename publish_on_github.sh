eca565f6dc2bb9c112d137364594e4dfac0fff79
Luca Lanziani <luca@lanziani.com>
2015-11-20 18:36:42 +0100

    Just to test travis

--- publish_on_github.sh
+++ publish_on_github.sh
@@ -24,5 +24,5 @@ ifeq ($(TRAVIS_PULL_REQUEST), false)
   # repo's gh-pages branch. (All previous history on the gh-pages branch
   # will be lost, since we are overwriting it.) We redirect any output to
   # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-  git push --force --quiet "https://${GH_TOKEN}@${GITHUB_REF}" master:master > /dev/null 2>&1
+  git push --force --quiet "https://${GH_TOKEN}@${GITHUB_REF}" master:master > /dev/null 2>&1 && echo "PUBLISHED!!!!"
 endif
