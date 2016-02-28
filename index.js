6f10a98e2b3eb45f3e348949fa32a9ab3e0dce38
gabrielem_ <gabriele.marazzi@gmail.com>
2016-02-28 22:22:21 +0100

    Add meetup icon

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var socials = {
   'youtube-square': 'https://www.youtube.com/channel/UCFm8OPi5USbFybw9SaTLxeA',
   'slack': 'https://romajs.herokuapp.com/',
   'github': 'https://github.com/Roma-JS',
-  'google-plus-square': 'https://plus.google.com/communities/114324393897443067092'
+  'google-plus-square': 'https://plus.google.com/communities/114324393897443067092',
+  'meetup': 'http://www.meetup.com/RomaJS/'
 };
 
 var Metalsmith  = require('metalsmith'),
@@ -49,8 +50,8 @@ var m = Metalsmith(__dirname)
     engine: 'handlebars'
   }))
   .use(assets({
-    source: 'src/assets', // relative to the working directory 
-    destination: './assets' // relative to the build directory 
+    source: 'src/assets', // relative to the working directory
+    destination: './assets' // relative to the build directory
   }))
   .use(assets({
     source: 'node_modules/font-awesome/fonts',
